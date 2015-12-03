d9f58c17240250468e415ac05ac78157b202275b
Thomas Karchesy <tkarchesyspam@gmail.com>
2015-12-03 09:25:35 -0800

    Don't know what I am doing.

---
+++ WebDeveloperEstimator/splash.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WebDeveloperEstimator
+{
+    partial class splash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(splash));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.pbxSplash = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSplash)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(56, 59);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(247, 24);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Web Developer Estimator";
+            this.lblTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(57, 236);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(303, 13);
+            this.lblDescription.TabIndex = 1;
+            this.lblDescription.Text = "A project from Web 105 and ITC110 at Seattle Central College.";
+            // 
+            // pbxSplash
+            // 
+            this.pbxSplash.InitialImage = ((System.Drawing.Image)(resources.GetObject("pbxSplash.InitialImage")));
+            this.pbxSplash.Location = new System.Drawing.Point(59, 114);
+            this.pbxSplash.Name = "pbxSplash";
+            this.pbxSplash.Size = new System.Drawing.Size(100, 50);
+            this.pbxSplash.TabIndex = 2;
+            this.pbxSplash.TabStop = false;
+            // 
+            // splash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 306);
+            this.ControlBox = false;
+            this.Controls.Add(this.pbxSplash);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblTitle);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "splash";
+            this.Load += new System.EventHandler(this.splash_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSplash)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.PictureBox pbxSplash;
+    }
+}
